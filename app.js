cec49ff250b64214ddfa8f37d0758d86bc51015d
Francesco Falanga <f.falanga@palmabit.com>
2015-10-11 14:38:06 +0200

    Initial commit.

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+var port = process.env.PORT || 1337;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(port, function () {
+  console.log('Listening on port ' + port);
+});
+
+app.post('/hello', function (req, res, next) {
+  var userName = req.body.user_name;
+  var botPayload = {
+    text : 'Hello ' + userName + ', welcome to Palmabit Slack channel!'
+  };
+  if (userName !== 'slackbot') {
+    return res.status(200).json(botPayload);
+  } else {
+    return res.status(200).end();
+  }
+});
